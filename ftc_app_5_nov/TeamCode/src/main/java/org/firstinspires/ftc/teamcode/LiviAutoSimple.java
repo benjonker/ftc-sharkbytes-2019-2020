3ed8a5772b103b7765a41f898a487ae93b3fa52e
P0vellesto <blueteagreensea@gmail.com>
2018-11-09 09:26:15 -0800

    Working on AUTO

---
+++ ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiviAutoSimple.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="AutoSample_Linear", group="Chris")
+public class AutoSample_Linear extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private DcMotor aDrive = null;
+    private DcMotor bDrive = null;
+    private DcMotor cDrive = null;
+    private DcMotor dDrive = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode() {
+        waitForStart();
+
+
+    }
+    public void turnLeft(){
+        telemetry.addData("status" , "turnLeft"); // Is the bot aligned with the gold mineral
+        telemetry.update();
+
+
+        aDrive.setPower(0.1);
+        bDrive.setPower(-0.1);
+
+    }
+
+    public void turnRight(){
+        telemetry.addData("status" , "turnRight"); // Is the bot aligned with the gold mineral
+        telemetry.update();
+
+
+        aDrive.setPower(-0.1);
+        bDrive.setPower(0.1);
+
+    }
+
+    public void forward(){
+        telemetry.addData("status" , "forward"); // Is the bot aligned with the gold mineral
+        telemetry.update();
+
+        aDrive.setPower(0.8);
+        bDrive.setPower(0.8);
+
+    }
+
+
+}
