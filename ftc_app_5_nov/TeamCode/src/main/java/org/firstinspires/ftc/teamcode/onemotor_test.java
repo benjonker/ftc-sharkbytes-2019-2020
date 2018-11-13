088b8ed860d1c8be7f4e0b7f88a5aaa7fafc4def
Nicholas Assaderaghi <nickassader@gmail.com>
2018-11-12 16:14:54 -0800

    hi

--- ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onemotor_test.java
+++ ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/onemotor_test.java
@@ -34,23 +34,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
-
-/**
- * This file provides basic Telop driving for a Pushbot robot.
- * The code is structured as an Iterative OpMode
- *
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
- *
- * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
- * It raises and lowers the claw using the Gampad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name="run_port_0")
 //@Disabled
@@ -72,19 +56,6 @@ public class onemotor_test extends LinearOpMode{
 
             port_0.setPower(-gamepad1.right_stick_y);
 
-
         }
-
-        // Instead of using the two lines of code below, you can just use my method.
-        //robot.leftDrive.setPower(left);
-        //robot.rightDrive.setPower(right);
-
-        // Use gamepad left & right Bumpers to open and close the claw
-
     }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-
 }
