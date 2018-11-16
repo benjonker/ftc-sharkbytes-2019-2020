1ba251c5528090fb794488b4c3a7435a156d99b4
P0vellesto <blueteagreensea@gmail.com>
2018-11-16 15:49:19 -0800

    First commit - Adding all of old Auto Code

---
+++ ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorAdafruit.java
@@ -0,0 +1,112 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+@Autonomous(name = "Sensor: AdafruitRGB", group = "Sensor")
+//@Disabled                            // Comment this out to add to the opmode list
+public class ColorSensorAdafruit extends LinearOpMode {
+
+    ColorSensor sensorRGB;
+    DeviceInterfaceModule cdim;
+
+    // we assume that the LED pin of the RGB sensor is connected to
+    // digital port 5 (zero indexed).
+    static final int LED_CHANNEL = 5;
+
+    @Override
+    public void runOpMode() {
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F,0F,0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        // bPrevState and bCurrState represent the previous and current state of the button.
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        // bLedOn represents the state of the LED.
+        boolean bLedOn = true;
+
+        // get a reference to our DeviceInterfaceModule object.
+        cdim = hardwareMap.deviceInterfaceModule.get("dim");
+
+        // set the digital channel to output mode.
+        // remember, the Adafruit sensor is actually two devices.
+        // It's an I2C sensor and it's also an LED that can be turned on or off.
+        cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannel.Mode.OUTPUT);
+
+        // get a reference to our ColorSensor object.
+        sensorRGB = hardwareMap.colorSensor.get("sensor_color");
+
+        // turn the LED on in the beginning, just so user will know that the sensor is active.
+        cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // loop and read the RGB data.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+        while (opModeIsActive())  {
+
+            // check the status of the x button on gamepad.
+            bCurrState = gamepad1.x;
+
+            // check for button-press state transitions.
+            if ((bCurrState == true) && (bCurrState != bPrevState))  {
+
+                // button is transitioning to a pressed state. Toggle the LED.
+                bLedOn = !bLedOn;
+                cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
+            }
+
+            // update previous state variable.
+            bPrevState = bCurrState;
+
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV((sensorRGB.red() * 255) / 800, (sensorRGB.green() * 255) / 800, (sensorRGB.blue() * 255) / 800, hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("LED", bLedOn ? "On" : "Off");
+            telemetry.addData("Clear", sensorRGB.alpha());
+            telemetry.addData("Red  ", sensorRGB.red());
+            telemetry.addData("Green", sensorRGB.green());
+            telemetry.addData("Blue ", sensorRGB.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
+
+            telemetry.update();
+        }
+
+        // Set the panel back to the default color
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+    }
+}
--- ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiviAutoSimple.java
+++ ftc_app_5_nov/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiviAutoSimple.java
@@ -5,55 +5,160 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.util.concurrent.TimeUnit;
+
+//TODO: We need to test this code, and get vuforia working.
 
 @Autonomous(name="AutoSample_Linear", group="Chris")
 public class LiviAutoSimple extends LinearOpMode {
 
     /* Declare OpMode members. */
-    private DcMotor aDrive = null;
-    private DcMotor bDrive = null;
-    private DcMotor cDrive = null;
-    private DcMotor dDrive = null;
+    private DcMotor mDrv_l0 = null;
+    private DcMotor mDrv_r0 = null;
+    private DcMotor mDrv_l1 = null;
+    private DcMotor mDrv_r1 = null;
+    private DcMotor mPin = null;
+    private Servo sArm = null;
+    //private Servo sBox = null;
+
     private ElapsedTime runtime = new ElapsedTime();
 
-    public void runOpMode() {
-        aDrive =  hardwareMap.get(DcMotor.class, "mDrv_l0");
-        bDrive = hardwareMap.get(DcMotor.class, "mDrv_r0");
-
-        //TEST
-        aDrive.setDirection(DcMotor.Direction.FORWARD);
-        cDrive.setDirection(DcMotor.Direction.FORWARD);
-
-
-    }
-    public void turnLeft(){
-        telemetry.addData("status" , "turnLeft"); // Is the bot aligned with the gold mineral
-        telemetry.update();
+    public final static double ARM_HOME = 0.0;
+    public final static double ARM_MIN_HOME = 0.0;
+    public final static double ARM_MAX_HOME = 1.0;
+    public final static double ARM_SPEED = 0.1;
+    public double armPosition = ARM_HOME;
+    ColorSensor colorSensor;
 
 
-        aDrive.setPower(0.1);
-        bDrive.setPower(-0.1);
-
+    public void stopMoving() {
+        mDrv_l0.setPower(0);
+        mDrv_l1.setPower(0);
+        mDrv_r0.setPower(0);
+        mDrv_r1.setPower(0);
     }
 
-    public void turnRight(){
-        telemetry.addData("status" , "turnRight"); // Is the bot aligned with the gold mineral
-        telemetry.update();
-
-
-        aDrive.setPower(-0.1);
-        bDrive.setPower(0.1);
+    public void turnLeft(double holdTime) {
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            mDrv_l0.setPower(0.1);
+            mDrv_r0.setPower(-0.1);
+            mDrv_l1.setPower(0.1);
+            mDrv_r1.setPower(-0.1);
+        }
+        stopMoving();
 
     }
 
-    public void forward(){
-        telemetry.addData("status" , "forward"); // Is the bot aligned with the gold mineral
-        telemetry.update();
+    public void turnRight(double holdTime) {
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            mDrv_l0.setPower(-0.1);
+            mDrv_r0.setPower(0.1);
+            mDrv_l1.setPower(-0.1);
+            mDrv_r1.setPower(0.1);
+        }
+        stopMoving();
+    }
 
-        aDrive.setPower(0.8);
-        bDrive.setPower(0.8);
+    public void forward (double holdTime) {
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            mDrv_l0.setPower(0.8);
+            mDrv_r0.setPower(0.8);
+            mDrv_l1.setPower(0.8);
+            mDrv_r1.setPower(0.8);
+        }
+        stopMoving();
+    }
 
+    public void backward(double holdTime) {
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            mDrv_l0.setPower(-0.8);
+            mDrv_r0.setPower(-0.8);
+            mDrv_l1.setPower(-0.8);
+            mDrv_r1.setPower(-0.8);
+        }
+        stopMoving();
+    }
+
+    public void turnServo(double holdTime){
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            sArm = hardwareMap.get(Servo.class, "arm");
+            armPosition += ARM_SPEED;
+            sArm.setPosition(armPosition);
+        }
+
+    }
+
+    public void jewel(double holdTime) throws InterruptedException{
+        ElapsedTime holdTimer = new ElapsedTime();
+        holdTimer.reset();
+        //TODO: Figure out what colors we need to look for, and then assign what to do when we see them
+        while (opModeIsActive() && holdTimer.time() < holdTime) {
+            if (colorSensor.blue() > colorSensor.red()) { //TODO: We need to get the rough RBG of each jewel and gold coin
+                turnRight(1);
+                forward(0.5);
+                telemetry.addData("BLUE", "%s visible");
+                //robot.armServo.setPosition(0.0);
+            } else {
+                forward(0.5);
+                turnLeft(1);
+                //Assuming that when you turn right for 1 sec,
+                //and you turn back left for 1 you will be in the original position
+                telemetry.addData("RED", "%s visible");
+                sArm.setPosition(ARM_HOME);
+            }
+
+        }
+    }
+
+    public void runOpMode() throws InterruptedException{
+        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+        colorSensor.enableLed(true);
+        mDrv_l0 = hardwareMap.get(DcMotor.class, "mDrv_l0");
+        mDrv_r0 = hardwareMap.get(DcMotor.class, "mDrv_r0");
+        mPin = hardwareMap.get(DcMotor.class, "mPin");
+        sArm = hardwareMap.get(Servo.class, "sArm");
+
+        mDrv_l1.setDirection(DcMotor.Direction.REVERSE);
+        mDrv_r1.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
+
+        //Unhooking the robot.
+        mPin.setPower(1);
+        TimeUnit.SECONDS.sleep(5);
+        mPin.setPower(0);
+        mDrv_l0.setPower(-0.1);
+        mDrv_l1.setPower(-0.1);
+        mDrv_r0.setPower(-0.1);
+        mDrv_r1.setPower(-0.1);
+        TimeUnit.SECONDS.sleep(5);
+        mDrv_l0.setPower(0);
+        mDrv_l1.setPower(0);
+        mDrv_r0.setPower(0);
+        mDrv_r1.setPower(0);
+        mPin.setPower(-1);
+        TimeUnit.SECONDS.sleep(5);
+
+
+        while (opModeIsActive()) {
+
+
+
+        }
     }
 
 
